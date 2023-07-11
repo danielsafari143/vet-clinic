dafec2567f0ca1ce08c9fc32af5000972305a6be
danielsafari143 <danielsafari143@gmail.com>
2023-07-11 15:41:43 +0200

    PROJECT SET UP

--- data.sql
+++ data.sql
@@ -1,13 +1,5 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
-(DEFAULT , 'Augmon' , ('2020-2-3'), 0 , true , 10.23);
-
-INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
-(DEFAULT , 'Gabumon' , ('2018-11-15'), 2 , true , 8);
-
-INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
-(DEFAULT , 'Pikachu' , ('2021-1-7'), 1 , false , 15.04);
-
-INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
-(DEFAULT , 'Devimon' , ('2017-5-12'), 5 , true , 11);
+INSERT INTO animals (name) VALUES ('Luna');
+INSERT INTO animals (name) VALUES ('Daisy');
+INSERT INTO animals (name) VALUES ('Charlie');
