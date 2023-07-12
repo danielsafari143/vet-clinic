348aa8cf437753c48db3bd2976c1b54df8236deb
danielsafari143 <danielsafari143@gmail.com>
2023-07-12 23:37:46 +0200

    ADD OWNER && SPECIES TABLE

--- schema.sql
+++ schema.sql
@@ -11,3 +11,30 @@ CREATE TABLE animals(
  species          VARCHAR(30)
  );
 
+CREATE TABLE species(
+id INT GENERATED ALWAYS AS IDENTITY,
+name VARCHAR(50) , 
+PRIMARY KEY(id)
+);
+
+CREATE TABLE owners(
+id INT GENERATED ALWAYS AS IDENTITY,
+full_name VARCHAR(50),
+age INT , 
+PRIMARY KEY(id)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+ 
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD CONSTRAINT species_id
+FOREIGN KEY (species_id) REFERENCES species (id);
+SELECT * FROM animals;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD CONSTRAINT owner_id
+FOREIGN KEY (owner_id) REFERENCES owners (id);
+SELECT * FROM animals;
