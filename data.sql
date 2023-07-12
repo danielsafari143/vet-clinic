4e1e9fe2bbde0165dc6597892d4792f5efd57496
danielsafari143 <danielsafari143@gmail.com>
2023-07-12 03:06:50 +0200

    UPDATE QUERIES FILE

--- data.sql
+++ data.sql
@@ -1,13 +1,22 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
-(DEFAULT , 'Augmon' , ('2020-2-3'), 0 , true , 10.23);
+(DEFAULT , 'Charmander' , ('2020-2-8'), 0 , false , -11);
 
 INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
-(DEFAULT , 'Gabumon' , ('2018-11-15'), 2 , true , 8);
+(DEFAULT , 'Plantmon' , ('2021-11-15'), 2 , true , -5.7);
 
 INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
-(DEFAULT , 'Pikachu' , ('2021-1-7'), 1 , false , 15.04);
+(DEFAULT , 'Squirtle' , ('1993-4-2'), 3 , false , -12.13);
 
 INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
-(DEFAULT , 'Devimon' , ('2017-5-12'), 5 , true , 11);
+(DEFAULT , 'Angemon' , ('2005-6-12'), 1 , true , -45);
+
+INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
+(DEFAULT , 'Boarmon' , ('2005-6-7'), 7 , true , 20.4);
+
+INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
+(DEFAULT , 'Blossom' , ('1998-10-13'), 3 , true , 17);
+
+INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
+(DEFAULT , 'Ditto' , ('2022-5-14'), 4 , true , 22);
--- queries.sql
+++ queries.sql
@@ -1,10 +1,31 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT *  FROM animals WHERE name LIKE '%mon';
-SELECT name  FROM animals WHERE date_of_birth BETWEEN '2016-1-1' AND '2019-12-31';
-SELECT name  FROM animals WHERE neutered = true AND escape_attempts < 3;
-SELECT date_of_birth  FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
-SELECT name , escape_attempts  FROM animals WHERE weight_kg > 10.5;
-SELECT * FROM animals WHERE neutered = true;
-SELECT * FROM animals WHERE name != 'Gabumon';
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+ BEGIN;
+ UPDATE animals SET species = 'unspecified';
+ ROLLBACK;
+
+ BEGIN;
+ UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+ UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+ COMMIT;
+
+ BEGIN;
+ DELETE FROM animals;
+ ROLLBACK;
+
+ BEGIN;
+ DELETE FROM animals WHERE date_of_birth > '2022-1-1';
+ SAVEPOINT deletedAnimals;
+ UPDATE animals SET weight_kg = weight_kg * -1;
+ ROLLBACK TO SAVEPOINT deletedAnimals;
+ UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+ COMMIT;
+
+ SELECT COUNT(*) FROM animals;
+ SELECT escape_attempts , COUNT(*) FROM animals GROUP BY escape_attempts HAVING escape_attempts = 0;
+ SELECT AVG(weight_kg) FROM animals;
+ SELECT  neutered , COUNT(escape_attempts) FROM animals GROUP BY neutered;
+ SELECT species , MIN(weight_kg) , MAX(weight_kg) FROM animals GROUP BY species;
+ SELECT species , AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-1-1' AND '2000-12-31'  GROUP BY species;
+ 
+
