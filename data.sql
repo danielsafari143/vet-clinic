821f657a0ba27f0a488f29f6663dd7aa1c873b6a
danielsafari143 <danielsafari143@gmail.com>
2023-07-13 03:00:55 +0200

    ADD QUERIES

--- data.sql
+++ data.sql
@@ -1,6 +1,18 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
+(DEFAULT , 'Augmon' , ('2020-2-3'), 0 , true , 10.23);
+
+INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
+(DEFAULT , 'Gabumon' , ('2018-11-15'), 2 , true , 8);
+
+INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
+(DEFAULT , 'Pikachu' , ('2021-1-7'), 1 , false , 15.04);
+
+INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
+(DEFAULT , 'Devimon' , ('2017-5-12'), 5 , true , 11);
+
+INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
 (DEFAULT , 'Charmander' , ('2020-2-8'), 0 , false , -11);
 
 INSERT INTO animals (id , name , date_of_birth , escape_attempts , neutered , weight_kg)  VALUES 
@@ -31,15 +43,10 @@ INSERT INTO owners VALUES (DEFAULT ,'Jodie Whittaker' ,  38);
 INSERT INTO species VALUES (DEFAULT , 'Pokemon');
 INSERT INTO species VALUES (DEFAULT , 'Digimon');
 
-UPDATE animals
-SET species_id =
-CASE
-WHEN name LIKE '%mon' THEN 'Digimon'
-ELSE 'Pokemon'
-END;
+UPDATE animals SET species_id = CASE WHEN name LIKE '%mon' THEN 'Digimon' ELSE 'Pokemon'END;
 
 UPDATE animals SET owner_id = 'Dean Winchester' WHERE name = 'Angemon' OR name = 'Boarmon';
 UPDATE animals SET owner_id = 'Melody Pond' WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
-UPDATE animals SET owner_id = 'Bob owns' WHERE name = 'Devimon' OR name = 'Plantmon';
+UPDATE animals SET owner_id = 'Bob' WHERE name = 'Devimon' OR name = 'Plantmon';
 UPDATE animals SET owner_id = 'Jennifer Orwell' WHERE name = 'Gabumon' OR name = 'Pikachu';
 UPDATE animals SET owner_id = 'Sam Smith' WHERE name = 'Agumon';
